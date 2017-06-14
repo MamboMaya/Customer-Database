19bd50324bef0826ab8a3963cc48666ed66bedcb
Damien Maya <damien_maya@yahoo.com>
2017-06-14 14:08:30 -0500

    oops

--- main.js
+++ main.js
@@ -1,40 +1,4 @@
-// // // 1. Fetch your users data
-// // // 2. Loop over the data
-// // // 3. Display it in the `.customers` element
-// function getCustomer() {
-//
-//   'use strict';
-//   fetch('https://randomuser.me/api/?format=json')
-//     .then( function(response){
-//       return response.json()
-//     })
-//     .then(function(json){
-//       // const info = data
-//       console.log("data", json)
-//     })
-//
-//
-//     //  const firstName = json.results[0].name.first
-//     //  const lastName = json.results[0].name.last
-//     //  const phone = json.cell
-//
-//      const html =
-//      `<div class="customer">
-//         <div class="name">
-//           ${results[0].name.firstName} + " " + ${results[0].name.lastName}
-//         </div>
-//         <div class="cellNumber">
-//         ${phone}
-//         </div>
-//       </div>`
-//
-//       document.querySelector(".customers").insertAdjacentHTML('beforeend', html)
-//
-// }
-//
-// for (var i = 1; i <=12; i++) {
-//   getCustomer(i)
-// }
+
 
   function getCustomer(){
 
